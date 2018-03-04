f3e8d6649fd50539a81744586877d69902805384
Sloan <gwaltneys2@winthrop.edu>
2018-03-04 02:50:56 -0500

    Added graphQL endpoint

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+// Server Imports
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 const port = process.env.PORT || 4000;
+// Middleware Imports
+const io = require('socket.io')(server);
+const graphQLExpress = require('express-graphql')
+
+app.use('/graph', graphQLExpress({
+  graphiql: true
+}))
 
 server.listen(port, ()=>{
   console.log(`API server waiting for request on port ${port}`);
