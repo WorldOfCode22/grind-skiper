8f172a4333e5eb4233fcfca7f002bda808ddf7f0
Sloan <gwaltneys2@winthrop.edu>
2018-03-04 17:00:57 -0500

    Added fix for uneeded cost doc id

--- models/cost-model.js
+++ models/cost-model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const assetModel = require('./asset-model');
 
 const cost = new mongoose.Schema({
+  _id: false,
   asset: assetModel.asset,
   quantity: Number
 })
